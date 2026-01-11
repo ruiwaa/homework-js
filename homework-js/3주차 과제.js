a1812197acc721ab1e61ec5fc1fe8a22e6e6fbac
ruiwaa <yeejii08@gmail.com>
2026-01-11 13:02:23 +0900

    문제풀이: 변수, 상수 작성 과제

---
+++ homework-js/3주차 과제.js
@@ -0,0 +1,13 @@
+// 변수, 상수 작성 과제
+let favoritemovie = '윤희에게'
+const LIGHTSPEED = '299,792,458 m/s'
+let is_complete_email_valificaiton = true
+let productInventory = 47 + '개'
+let memberPoing = 15_8000 + '점'
+const APIBASICSEVER = 'https://api.example.com'
+let postView = 1_234
+let deliveryStatus = '배송중'
+let can_use_coupon = true
+const MAXIMUMUPLOADFILESIZE =  10485760
+let UserRatingScore = parseFloat(85.5) + '점'
+let agree_receive_notifications = true
