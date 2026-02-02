2d92a1434c699a291cecca79a4f4812255e1a4be
ruiwaa <yeejii08@gmail.com>
2026-02-02 20:51:53 +0900

    추가: 5주차 과제 회고 작성

--- homework-js/plan-card-component-project/assets/scripts/main.js
+++ homework-js/plan-card-component-project/assets/scripts/main.js
@@ -9,7 +9,7 @@
  */
 
 
-// 변수 선언
+// 1 단계: 팔요한 변수값 할당
 const planCards =document.querySelectorAll('.plan-card')
 const planCard = document.querySelector('.plan-card')
 const priceCard1 = document.querySelector('[data-plan="price-card-1"]')
@@ -21,39 +21,61 @@ const selectedOption = document.querySelector('.selected-option')
 
 
 
-// plan-price 가격 쉼표 추가 
+// 2 단계: plan-price 가격 쉼표 추가 
 for(const price of priceNumber){
   const changeNum = Number(price.textContent)
   if(!isNaN(changeNum) ){
     price.textContent = '₩' + changeNum.toLocaleString()
   } 
   
-  
 } 
 
-// 플랜 카드 이벤트 핸들러 함수 (클릭 시 나올 이벤트)
-const handleSelect = (card) => {
-  const icon = card.previousElementSibling
+// 3 단계: 함수 생성
+// 1) 이전에 선택된 카드 비활성화 함수 만들기
+const removeSelected = (card) => {
 
-  // 선택되지 않은 요소 비활성화
+  //카드와 아이콘에 클래스명 제거
+  card.classList.remove('active')
+  const icon = card.previousElementSibling
+  if(icon) icon.classList.remove('active')
+
+}
+
+// 2) 속성 제어 함수 만들기 
+// 추가
+function setAttr(element, attrName, attrValue){
+  element.setAttribute(attrName, attrValue)
+
+}
+// 삭제
+function removeAttr(element, attrName, attrValue){
+  if(attrValue === null){
+    element.removeAttribute(attrName)
+  }
+}
+
+// 3) 플랜 카드 클릭 이벤트 핸들러 함수 
+const handleSelect = (card) => {
+const icon = card.previousElementSibling
+
+  // 3-1 선택되지 않은 플랜 카드 비활성화
   planCards.forEach((selectedCard) => {
     if(selectedCard !== card) {
       removeSelected(selectedCard)
     }
   })
 
-  // 현재 카드 활성화되었을 시 추가될 기능들
-  //1. 플랜 카드에 active 클래스명 추가
+  // 3-2 현재 플랜 카드 활성화되었을 시 추가될 기능들
+  //3-2-1 플랜 카드에 active 클래스명 추가
   card.classList.add('active')
-  //2. 아이콘에 active 클래스명 추가
+  //3-2-2 아이콘에 active 클래스명 추가
   icon.classList.add('active')
-  //3. 플랜 카드에 속성 쓰기
+  //3-2-3 플랜 카드에 속성 설정(아티클에 탭속성 추가)
   setAttr(card, 'tabindex', '0') 
-  //4. 선택 버튼 클릭 후 옵션 선택 텍스트의 hidden 속성 삭제
+  //3-2-4 선택 버튼 클릭 후 옵션 선택 텍스트의 hidden 속성 삭제
   removeAttr(selectedOption,'hidden',null) 
   
-
-  // 버튼에 aria-pressed 속성 추가
+  //3-2-5 버튼에 aria-pressed 속성 추가
   const button = card.querySelector('.plan-price-button')
   if(button) {
   setAttr(button, 'aria-pressed', 'true')
@@ -61,8 +83,11 @@ const handleSelect = (card) => {
   
 }
 
+// 4) 플랜 카드 선택시 나타내는 선택된 옵션 박스 함수
+// 기능: 각 플랜카드 선택 시 다른 텍스트가 표시 되어야 함
+
 const showOptionInfo = function (card){
-    // 여기서 각 플랜카드 선택 시 다른 텍스트가 표시 되어야 함
+  // 필요한 변수 할당
       const cardParentElement = card.parentElement
       const wrpperParentElement = cardParentElement.parentElement
       const optionInfo = wrpperParentElement.nextElementSibling
@@ -73,6 +98,7 @@ const showOptionInfo = function (card){
       const strong = optionInfo.querySelector('#plan-membership')
       const span = optionInfo.querySelector('[data-type="plan-membership"]')
 
+      // 해당 아티클 요소 선택 시 해당 멤버쉽 등급과 가격이 보이도록 설정
       strong? (strong.textContent = `${membershipValue}`): null
       span? span.textContent = `플랜(${priceValue})을 선택하셨습니다.` : null
 
@@ -80,51 +106,29 @@ const showOptionInfo = function (card){
     }
 
     
-// 활성화 클래스 추가 했을 때 발생하는 이벤트 함수들 연결
-//클릭 및 엔터 
+// 4단계: 활성화 클래스 추가 했을 때 발생하는 이벤트 함수들 연결
+// 클릭 및 엔터 
 const active = () => {
   planCards.forEach((card) => {
 
-    // 클릭 이벤트에 handleSelect 함수 연결
+    // 클릭 이벤트에 handleSelect & showOptionInfo 함수 연결
     card.addEventListener('click', (e) => {
       handleSelect(e.currentTarget)
       showOptionInfo(e.currentTarget)
     }    
-   , {capture: true})
+   , {capture: true}) // 버튼을 클릭했을 때 아티클 요소까지 클릭이 될 수 있게 캡쳐링 사용
 
    
 
-   // 엔터키 이벤트도 추가
+   // 엔터키 이벤트에 handleSelect & showOptionInfo 함수 연결
     card.addEventListener('keydown', (e) => {
       if(e.key === 'Enter') {
         handleSelect(e.currentTarget)
         showOptionInfo(e.currentTarget)
       }
     })
-
 })
 }
 // 함수 실행
 active()
 
-// 이전에 선택된 카드 비활성화 함수 만들기
-const removeSelected = (card) => {
-
-  //카드와 아이콘에 클래스명 제거
-  card.classList.remove('active')
-  const icon = card.previousElementSibling
-  if(icon) icon.classList.remove('active')
-
- 
-}
-
-// 속성 제어 함수 만들기 (추가/삭제)
-function setAttr(element, attrName, attrValue){
-  element.setAttribute(attrName, attrValue)
-
-}
-function removeAttr(element, attrName, attrValue){
-  if(attrValue === null){
-    element.removeAttribute(attrName)
-  }
-}
